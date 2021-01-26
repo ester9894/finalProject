fbcc5609ca86b9c35859934375dd24c5438a5eb7
ester9894 <esterika246@gmail.com>
2021-01-26 10:32:39 +0200

    עדכון משילת לקראת סיום

--- SERVER/DAL/Account.cs
+++ SERVER/DAL/Account.cs
@@ -28,7 +28,6 @@ namespace DAL
         public string Password { get; set; }
         public long ManagerId { get; set; }
     
-        public virtual User User { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TypesList> TypesLists { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
@@ -37,5 +36,6 @@ namespace DAL
         public virtual ICollection<Product> Products { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<FollowUpList> FollowUpLists { get; set; }
+        public virtual User User { get; set; }
     }
 }
